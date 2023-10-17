8fed2e91944a7aa3f2d6d6578f64c04455f49e96
Rahmad Fani <rahmadfani46@gmail.com>
2023-10-17 14:35:21 +0700

    Initial Commit

---
+++ MyLastDev/MyLastDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLastDevApp.swift
+//  MyLastDev
+//
+//  Created by Fani on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MyLastDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
